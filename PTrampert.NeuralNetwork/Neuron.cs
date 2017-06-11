a2f96552567a617e80e899a522f7c5bd767130ee
Paul Trampert <paul.trampert@gmail.com>
2017-06-11 14:24:03 -0400

    Add support for setting the activation function of a neuron.

--- PTrampert.NeuralNetwork/Neuron.cs
+++ PTrampert.NeuralNetwork/Neuron.cs
@@ -7,18 +7,25 @@ namespace PTrampert.NeuralNetwork
     public class Neuron
     {
         public List<double> Weights { get; set; }
+        public ActivationFunction ActivationFunction { get; set; }
         public double P { get; set; }
         public double Delta { get; set; }
         public double Output { get; set; }
         public double LearningRate { get; set; }
+
+        private static readonly Dictionary<ActivationFunction, FuncAndDeriv> ActivationFunctions = new Dictionary<ActivationFunction, FuncAndDeriv>
+        {
+            { ActivationFunction.Sigmoid, new FuncAndDeriv { Func = Sigmoid, Deriv = SigmoidPrime } }
+        };
 
         public Neuron()
         {
             
         }
 
-        public Neuron(int inputs, double learningRate = .1, double p = 1, Random rand = null)
+        public Neuron(int inputs, ActivationFunction activationFunction = ActivationFunction.Sigmoid, double learningRate = .1, double p = 1, Random rand = null)
         {
+            ActivationFunction = activationFunction;
             var random = rand ?? new Random();
             Weights = new int[inputs].Select(i => 2 * random.NextDouble() - 1).ToList();
             LearningRate = learningRate;
@@ -27,12 +34,12 @@ namespace PTrampert.NeuralNetwork
 
         public double Think(List<double> inputs)
         {
-            return Output = Sigmoid(inputs.Select((n, i) => n * Weights[i]).Sum());
+            return Output = ActivationFunctions[ActivationFunction].Func(inputs.Select((n, i) => n * Weights[i]).Sum(), P);
         }
 
         public double CalculateDelta(List<double> inputs, double error)
         {
-            return Delta = error * SigmoidPrime(Output);
+            return Delta = error * ActivationFunctions[ActivationFunction].Deriv(Output);
         }
 
         public void UpdateWeights(List<double> inputs)
@@ -40,14 +47,14 @@ namespace PTrampert.NeuralNetwork
             Weights = Weights.Select((w, i) => w + (LearningRate * Delta * inputs[i])).ToList();
         }
 
-        private double SigmoidPrime(double output)
+        private static double SigmoidPrime(double output)
         {
             return output * (1 - output);
         }
 
-        private double Sigmoid(double weightedSum)
+        private static double Sigmoid(double weightedSum, double p)
         {
-            return 1 / (1 + Math.Exp(-weightedSum/P));
+            return 1 / (1 + Math.Exp(-weightedSum/p));
         }
 
         public Neuron Clone(Random rand = null)
@@ -56,8 +63,20 @@ namespace PTrampert.NeuralNetwork
             {
                 Weights = Weights,
                 LearningRate = LearningRate,
+                ActivationFunction = ActivationFunction,
                 P = P
             };
         }
+
+        private class FuncAndDeriv
+        {
+            public Func<double, double, double> Func { get; set; }
+            public Func<double, double> Deriv { get; set; }
+        }
+    }
+
+    public enum ActivationFunction
+    {
+        Sigmoid,
     }
 }
